5794a396f19f315aa7890be2e2e39029fd4b4fa0
Anthony <zatgaming@gmail.com>
2019-08-27 11:06:47 -0600

    starting repo

---
+++ printer_sync.py
@@ -0,0 +1,18 @@
+import requests
+
+
+
+def check_drive():
+    # Check drive vs local to see if there are any differences that need synced.
+    # We will always exclude ".metadata.json" as this is really printer specific.
+    pass
+
+
+def check_printer():
+    # Make sure the printer isn't running.
+    pass
+
+
+
+if __name__ == '__main_+_':
+    pass
