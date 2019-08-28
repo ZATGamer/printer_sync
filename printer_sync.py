548ef350d0700a380152a20b9366b1a32e2573f7
Anthony <zatgaming@gmail.com>
2019-08-28 09:27:41 -0600

    Some useful messages

--- printer_sync.py
+++ printer_sync.py
@@ -22,22 +22,25 @@ def safe_to_sync():
     r_data = requests.get('http://localhost/api/printer', proxies=g_proxys)
     if r_data.status_code == 409:
         # 403 means octoprint is not even connected to the printer. Safe to update files
-        print("got 409")
+        print("Printer is Offline (http 409).")
         return True
     if r_data.status_code == 200:
-        print("got 200")
+        print("Printer is Online (http 200).")
         # Convert string to json
         j_data = json.loads(r_data.text)
         # OK now get weather we are printing or not
         if j_data['state']['text'] == 'Operational':
+            print('Printer is in an "Operational" state.')
             return True
         else:
+            print('Printer is NOT in an "Operational" state.')
             return False
 
 
 def sync():
     print("Sync Happening")
     os.system('rclone sync {} {} --exclude {} --exclude {}'.format(g_drive, local_drive, exclude1, exclude2))
+    print("Sync Complete")
 
 
 if __name__ == '__main__':
@@ -49,5 +52,7 @@ if __name__ == '__main__':
     g_proxys = {
     }
     if safe_to_sync():
+        print("Safe to Sync = True")
         if needs_update():
+            print("Files are out of sync.")
             sync()
